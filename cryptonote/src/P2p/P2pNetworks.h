9fd9e849259eb62b4215362d93c79f9ce788a58b
theboldtoken <dev1@boldtoken.io>
2018-06-29 23:05:27 -0600

    Initial commit

---
+++ cryptonote/src/P2p/P2pNetworks.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+	// Bold Official Network
+	const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x19, 0x8a, 0x59, 0x14, 0x3d, 0x4f, 0x2e, 0x9c, 0x23, 0x44, 0x01, 0x42, 0x40, 0x61, 0x65, 0x31 } };
+}
