bc03be64dd2ec109c258e07ed282210336a3ae08
theboldtoken <35311150+theboldtoken@users.noreply.github.com>
2018-07-02 05:24:23 -0600

    Update CryptoNoteConfig.h

--- cryptonote/src/CryptoNoteConfig.h
+++ cryptonote/src/CryptoNoteConfig.h
@@ -118,8 +118,8 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  128;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              =  39564;
-const int      RPC_DEFAULT_PORT                              =  39565;
+const int      P2P_DEFAULT_PORT                              =  53940;
+const int      RPC_DEFAULT_PORT                              =  53941;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
@@ -138,9 +138,9 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[] = "";
 
 const char* const SEED_NODES[] = {
-	"seed1.boldprivate.network:39564",
-	"seed2.boldprivate.network:39564",
-	"pool.boldprivate.network:39564"
+	"seed1.boldprivate.network:53940",
+	"seed2.boldprivate.network:53940",
+	"pool.boldprivate.network:53940"
 };
 
 struct CheckpointData {
