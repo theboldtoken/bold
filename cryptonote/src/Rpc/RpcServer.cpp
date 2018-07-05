e773071e06e3b6bd0adc5fe487d58e7d644ee90b
theboldtoken <35311150+theboldtoken@users.noreply.github.com>
2018-07-04 21:52:05 -0600

    RPCServer - include difficulty value

--- cryptonote/src/Rpc/RpcServer.cpp
+++ cryptonote/src/Rpc/RpcServer.cpp
@@ -66,6 +66,7 @@ void serialize(BlockShortInfo& blockShortInfo, ISerializer& s) {
   KV_MEMBER(blockShortInfo.blockId);
   KV_MEMBER(blockShortInfo.block);
   KV_MEMBER(blockShortInfo.txPrefixes);
+  KV_MEMBER(blockShortInfo.difficulty);	
 }
 
 namespace {
@@ -453,6 +454,7 @@ bool RpcServer::f_on_blocks_list_json(const F_COMMAND_RPC_GET_BLOCKS_LIST::reque
 		block_short.height = i;
 		block_short.hash = Common::podToHex(block_hash);
 		block_short.tx_count = blk.transactionHashes.size() + 1;
+		block_short.difficulty = blkDetails.difficulty
 
 		res.blocks.push_back(block_short);
 
