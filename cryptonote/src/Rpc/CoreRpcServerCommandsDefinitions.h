60252ff20f98a085d0aafa9a6bfec1c7d38ab477
enkrypter <30854882+enkrypter@users.noreply.github.com>
2018-07-05 00:23:28 -0600

    Update CoreRpcServerCommandsDefinitions.h

--- cryptonote/src/Rpc/CoreRpcServerCommandsDefinitions.h
+++ cryptonote/src/Rpc/CoreRpcServerCommandsDefinitions.h
@@ -478,6 +478,7 @@ struct f_block_short_response {
 	std::string hash;
 	uint64_t tx_count;
 	uint64_t cumul_size;
+	uint64_t difficulty;
 
 	void serialize(ISerializer &s) {
 		KV_MEMBER(timestamp)
